507342abf7fea70b8be4f2a1c4d9cbbaac5d55c1
Clemens Lange <clemens.lange@cern.ch>
2016-03-12 18:45:54 +0100

    make scripts a bit smarter:

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ echo "running setup.sh"
 DOTDIR=".dotfiles"
 
 platform=$(uname | awk '{print tolower($0)}')
+
 [[ -f "./${DOTDIR}/setup_${platform}.sh" ]] && source "./${DOTDIR}/setup_${platform}.sh"
 
-[[ -f "./${DOTDIR}/system_${platform}.sh" ]] && source "./${DOTDIR}/system_${platform}.sh"
+if [[ -f "./${DOTDIR}/system_${platform}.installed" ]]; then
+  echo "System settings already adjusted" && return
+else
+  touch "./${DOTDIR}/system_${platform}.installed"
+  [[ -f "./${DOTDIR}/system_${platform}.sh" ]] && source "./${DOTDIR}/system_${platform}.sh"
+fi
